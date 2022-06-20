7f839812a382daae2241bd543e4f4ace03fe969f
nishanmazumder <arosh019@gmail.com>
2022-06-21 02:30:48 +0600

    email validation added

--- projects/email-validator.py
+++ projects/email-validator.py
@@ -1,3 +1,34 @@
+email = 'test@test.com'
+
+
+# validation = {
+#     '@': True if '@' in email and email.count('@') == 1 else False,
+#     '.': True if '.' in email and ((email[-3] == '.') ^ (email[-4] == '.')) else False,
+#     'space': [x for x in email if x == x.isspace()]
+# }
+
+email_space = 0
+
+validation = {
+    '@': True if '@' in email and email.count('@') == 1 else False,
+    '.': True if '.' in email and ((email[-3] == '.') ^ (email[-4] == '.')) else False,
+}
+
+if len(email) >= 6:  # 1
+    if email[0].isalpha():  # 2
+        if (validation.get('@') and validation.get('.')) == True:
+            for x in email:
+                if x == x.isspace():
+                    email_space = 1
+            print(email_space)
+        else:
+            print("wrong email 3!")
+    else:
+        print("wrong email 2!")
+else:
+    print("wrong email 1!")
+
+
 # Package: https://pypi.org/project/email-validator/
 
 # from email_validator import (
@@ -14,7 +45,7 @@
 #     #     print('failed!')
 
 #     print('done')
-        
+
 # except EmailNotValidError as e:
 #    # Email not valid. This will be an EmailSyntaxError if the form of the address is invalid or an EmailUndeliverableError if the domain does not resolve.
 #     print(str(e))
