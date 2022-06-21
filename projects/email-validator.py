06becd6f5a0762b678afc5a92ac8f5ac20cde01b
nishanmazumder <arosh019@gmail.com>
2022-06-21 10:52:54 +0600

    Regex added

--- projects/email-validator.py
+++ projects/email-validator.py
@@ -1,18 +1,33 @@
+import re
+
 email = 'test@test.com'
 
-validation = {
-    '@': True if '@' in email and email.count('@') == 1 else False,
-    '.': True if '.' in email and ((email[-3] == '.') ^ (email[-4] == '.')) else False
-}
+# Regex
+email_condition = '^[a-z 0-9]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$'
+# email_condition = '^[a-z 0-9]+(?:[._][a-z0-9]+)*@(?:\w+\.)+\w{2,3}$' // https://regex101.com/r/UxYi3A/1
 
-if len(email) >= 6 and ' ' not in email:  # 1
-    if email[0].isalpha():  # 2
-        if (validation.get('@') and validation.get('.')) == True:
-           print('Success!')
-    else:
-        print("wrong email 2!")
+if re.search(email_condition, email):
+    print('Success!')
 else:
-    print("wrong email 1!")
+    print("wrong email!")
+
+
+# Manual
+# validation = {
+#     '@': True if '@' in email and email.count('@') == 1 else False,
+#     '.': True if '.' in email and ((email[-3] == '.') ^ (email[-4] == '.')) else False
+# }
+
+# if len(email) >= 6 and ' ' not in email:  # 1
+#     if email[0].isalpha():  # 2
+#         if (validation.get('@') and validation.get('.')) == True:
+#            print('Success!')
+#     else:
+#         print("wrong email 2!")
+# else:
+#     print("wrong email 1!")
+
+
 
 
 # Package: https://pypi.org/project/email-validator/
