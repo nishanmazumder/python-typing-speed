99e214bebea573f0845aac08a546f55053ea69dc
nishanmazumder <arosh019@gmail.com>
2022-06-25 15:47:31 +0600

    text2speech and desktop notification done

---
+++ projects/notification/notification.py
@@ -0,0 +1,19 @@
+from email import message
+from turtle import title
+from plyer import notification
+import time
+
+notify_text = "Hi, i am reminder. please smoke now!"
+
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title = '*** SMOKE TIME ***',
+            message = notify_text,
+            app_icon = 'D:/laragon/www/python/projects/notification/notify.ico',
+            timeout = 5
+        )
+
+        time.sleep(20)
+
+# Run background pythonw notification.py        
--- projects/youtubetdownloader.py
+++ projects/youtubetdownloader.py
@@ -1,9 +1,34 @@
 from pytube import YouTube
+from pytube import Playlist
 
 vid = 'https://www.youtube.com/watch?v=nd7JrEcnJaw'
 
 yt = YouTube(vid)
 
-print(yt.title)
+vid_title = yt.title
+vid_thumb = yt.thumbnail_url
+vid_streams = yt.streams.all()
+# vid_streams = yt.streams.filter(only_audio=True) # For audio only
 
-# print(dir(YouTube))
+# for i, stream in enumerate(vid_streams):
+#     print(i, stream)
+
+# print()
+
+# vid_download = (int(input('Enter the number: ')))
+
+# vid_streams[vid_download].download()
+# print('Video downloaded!')
+
+######### PLAYLIST ###########
+yt_playlist = Playlist('Link of playlist')
+
+print(f'Downloading: {yt_playlist.title}')
+
+for video in yt_playlist.videos:
+    video.streams.first().download() #get_by_resolution
+
+print('Playlist downloaded!')
+
+# print(dir(yt.streams))
+
